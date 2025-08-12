81b43ab1e4412e989299d3853d975b35d52b15cb
m4rllon <marllonpc33@gmail.com>
2025-08-12 19:31:54 -0300

    refactor: delete MyTitle and MyText components

--- src/components/MyText/styles.ts
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components/native";
-
-export const Text = styled.Text`
-    color: ${({theme}) => theme.colors.text_heading};
-    font-family: ${({theme}) => theme.fonts.regular};
-    font-size: 16px;
-    line-height: 24;
-`
--- src/components/MyTitle/styles.ts
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components/native";
-
-export const Title = styled.Text`
-    color: ${({theme}) => theme.colors.text_heading};
-    font-family: ${({theme}) => theme.fonts.bold};
-    font-size: 24px;
-    line-height: 30;
-`
